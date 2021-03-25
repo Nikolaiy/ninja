042c5e114c3b58cae04922a3178cecbfa76c9835
Nikolay <30789678+Nikolaiy@users.noreply.github.com>
2021-03-25 10:18:39 +0500

    added menu mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,37 +3,49 @@ import PropTypes from "prop-types";
 
 import Menu from './Menu'
 
-import {Logo, ButtonAuth, City} from './common';
+import { Logo, ButtonAuth, City } from './common';
 
 function Header(props) {
-  return (
-    <div className="wrapper">
-      <div className="container">
-        <div className="header-top">
-          <div className="header-wrap">
-            <Logo />
-            <div className="header__info">
-              <div className="header-wrap__delivery">
-                <City />
-                <p>
-                  Среднее время доставки 35 минут{" "}
-                  <span> 10% скидка на самовынос </span>
-                </p>
-              </div>
-              <div className="header-wrap__contact">
-                <a className="header-wrap__phone" href="tel:8800000000">
-                  8(495) 143-43-03
+
+	const [activeMenu, setActiveMenu] = React.useState(false)
+
+	const handleClick = () => {
+		setActiveMenu(!activeMenu)
+	}
+
+	console.log(activeMenu)
+
+	return (
+		<div className="wrapper">
+			<div className="container">
+				<div className="header-top">
+					<div className="header-wrap">
+						<Logo />
+						<div className="header__info">
+							<div className="header-wrap__delivery">
+								<City />
+								<p>
+									Среднее время доставки 35 минут{" "}
+									<span> 10% скидка на самовынос </span>
+								</p>
+							</div>
+							<div className="header-wrap__contact">
+								<a className="header-wrap__phone" href="tel:8800000000">
+									8(495) 143-43-03
                 </a>
-                <p> Ежедневно с 10.00 до 23.00 </p>
-              </div>
-            </div>
-          </div>
-          <ButtonAuth />
-          <Menu />
-        </div>
-      </div>
-    </div>
-  );
+								<p> Ежедневно с 10.00 до 23.00 </p>
+							</div>
+						</div>
+					</div>
+					<ButtonAuth />
+					<div className="burger__wrap" onClick={handleClick}>
+						<span className="burger__menu"></span>
+					</div>
+					<Menu active={activeMenu} />
+				</div>
+			</div>
+		</div>
+	);
 }
 
 Header.propTypes = {};
